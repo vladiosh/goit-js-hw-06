0e9de5d5b20db37e6369f0210c9f7fda44cb6daa
VLADYSLAV FEDCHENKO <vladfedcenko33@gmail.com>
2022-09-09 15:36:07 +0300

    create task-3 and task-4

---
+++ src/js/task-04.js
@@ -0,0 +1,17 @@
+let counterValue = 0;
+
+const refs = {
+  decrementBtn: document.querySelector('button[data-action="decrement"]'),
+  incrementBtn: document.querySelector('button[data-action="increment"]'),
+  valueEl: document.querySelector("#value"),
+};
+
+refs.decrementBtn.addEventListener("click", () => {
+  counterValue -= 1;
+  refs.valueEl.textContent = counterValue;
+});
+
+refs.incrementBtn.addEventListener("click", () => {
+  counterValue += 1;
+  refs.valueEl.textContent = counterValue;
+});
