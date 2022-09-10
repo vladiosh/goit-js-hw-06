67812e15a9bc39e6359007226a0a8c21f88610b0
VLADYSLAV FEDCHENKO <vladfedcenko33@gmail.com>
2022-09-10 14:56:32 +0300

    create task-8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Заповніть пусті поля!");
+    return;
+  }
+  const userData = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(userData);
+
+  event.currentTarget.reset();
+});
