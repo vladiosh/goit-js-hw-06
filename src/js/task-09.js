53a9caaf653a7b0a92706c5a7bca5c3c6b15eefe
VLADYSLAV FEDCHENKO <vladfedcenko33@gmail.com>
2022-09-10 17:21:03 +0300

    create task-9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  changeColorBtn: document.querySelector(".change-color"),
+  color: document.querySelector(".color"),
+  body: document.querySelector("body"),
+};
+refs.changeColorBtn.addEventListener("click", () => {
+  const currentColor = getRandomHexColor();
+  refs.color.textContent = currentColor;
+  refs.body.style.backgroundColor = currentColor;
+});
